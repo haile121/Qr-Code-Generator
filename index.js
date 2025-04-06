5f48fb9b48644b5d9a8ccb86be090cb4a86d9e93
haile121 <haileag8@gmail.com>
2025-04-06 20:10:37 +0300

    index.js

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ import qr from "qr-image";
 import bodyParser from "body-parser";
 
 const app = express();
-const PORT = 3000;
 
-app.use(cors()); // Allow requests from frontend
-app.use(bodyParser.json()); // Parse JSON body
+//const PORT = 3000;
 
-// Generate QR Code API
+//new
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(bodyParser.json());
+
 app.post("/generate", (req, res) => {
   const { url } = req.body;
 
@@ -20,12 +24,23 @@ app.post("/generate", (req, res) => {
   try {
     const qr_svg = qr.image(url, { type: "png" });
     res.setHeader("Content-Type", "image/png");
-    qr_svg.pipe(res); // Send the PNG image as response
+    qr_svg.pipe(res);
   } catch (error) {
     res.status(500).json({ error: "QR generation failed" });
   }
 });
 
-app.listen(PORT, () => {
+//new2
+export default app;
+
+/* app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-});
+}); */
+
+//new3
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Local server running on http://localhost:${PORT}`);
+  });
+}
